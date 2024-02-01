8b4a8734b60e40012c924225f96568422bdf4be7
VismayaUnnikrishnan <119035329+VismayaUnnikrishnan@users.noreply.github.com>
2024-02-01 20:13:18 +0530

    Add files via upload

---
+++ ecom/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=40)
+    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
+    price = models.PositiveIntegerField()
+    description=models.CharField(max_length=40)
+    def __str__(self):
+        return self.name
+        
---
+++ ecom/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect,reverse
+from . import models
+from django.http import HttpResponseRedirect,HttpResponse
+from django.core.mail import send_mail
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib import messages
+from django.conf import settings
+
+
+# Create your views here.
+def home_view(request):
+    products=models.Product.objects.all()
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        counter=product_ids.split('|')
+        product_count_in_cart=len(set(counter))
+    else:
+        product_count_in_cart=0
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'ecom/index.html',{'products':products,'product_count_in_cart':product_count_in_cart})
